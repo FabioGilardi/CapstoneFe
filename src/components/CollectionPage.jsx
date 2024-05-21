f9c51281b0422027268dbe00afd9b7896ea9576b
Fabio Gilardi <fabiogilardi02@gmail.com>
2024-05-21 17:02:06 +0200

    filter on collectionPage added and working

--- src/components/CollectionPage.jsx
+++ src/components/CollectionPage.jsx
@@ -1,9 +1,10 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import CarCard from "./CarCard";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { saveCars } from "../redux/actions";
 import { Spinner } from "react-bootstrap";
 import quality from "../assets/images/quality logo.png";
@@ -16,6 +17,26 @@ const CollectionPage = () => {
   // const accessToken = useSelector((state) => state.authReducer.accessToken);
   const carList = useSelector((state) => state.carReducer.cars);
   const isLoadingCars = useSelector((state) => state.carReducer.isLoadingCars);
+
+  // LOCAL STATE
+  const [filter, setFilter] = useState("");
+
+  // OTHER FUNCTIONS
+  const carFilter = (arr, str) => {
+    if (str !== "") {
+      return arr.content.filter((car) => car.brand === str);
+    } else return arr.content;
+  };
+
+  const handleFilter = (str) => {
+    setFilter(str);
+  };
+
+  const stringManipulation = (string) => {
+    if (string !== "BMW") {
+      return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
+    } else return string;
+  };
 
   // COMPONENT MOUNT/UPDATE
   useEffect(() => {
@@ -90,14 +111,86 @@ const CollectionPage = () => {
                 <img src={quality} width="55%" className="rounded-circle"></img>
               </Col>
             </Row>
+            <Row className={filter === "" ? "mt-4 mb-5" : "mt-4"}>
+              <Col
+                xs={6}
+                lg={3}
+                className="d-flex align-items-center justify-content-center"
+              >
+                <img
+                  src="https://cdn.icon-icons.com/icons2/1834/PNG/512/iconfinderbmwlogo4140436-115966_115915.png"
+                  width={70}
+                  className="star"
+                  onClick={() => {
+                    handleFilter("BMW");
+                  }}
+                ></img>
+              </Col>
+              <Col
+                xs={6}
+                lg={3}
+                className="d-flex align-items-center justify-content-center"
+              >
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/it/thumb/a/a1/Logo_della_Porsche.svg/1589px-Logo_della_Porsche.svg.png"
+                  width={55}
+                  className="star"
+                  onClick={() => {
+                    handleFilter("PORSCHE");
+                  }}
+                ></img>
+              </Col>
+              <Col
+                xs={6}
+                lg={3}
+                className="d-flex align-items-center justify-content-center mt-2 mt-lg-0"
+              >
+                <img
+                  src="https://uploads.audi-mediacenter.com/system/production/media/116125/images/74022a65b478f7b3a8e0bf8ba70994f66fde5dd7/A231415_web_2880.jpg?1698528985"
+                  width={110}
+                  className="star"
+                  onClick={() => {
+                    handleFilter("AUDI");
+                  }}
+                ></img>
+              </Col>
+              <Col
+                xs={6}
+                lg={3}
+                className="d-flex align-items-center justify-content-center mt-2 mt-lg-0"
+              >
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Mercedes-Benz_Star_2022.svg/800px-Mercedes-Benz_Star_2022.svg.png"
+                  width={70}
+                  className="star"
+                  onClick={() => {
+                    handleFilter("MERCEDES");
+                  }}
+                ></img>
+              </Col>
+              {filter !== "" && (
+                <Col xs={12} className="px-0 ms-lg-4 px-lg-5 mt-4">
+                  <Button
+                    variant="white"
+                    className="d-flex align-items-center border border-secondary rounded-pill mb-4 carPageBack"
+                    onClick={() => {
+                      handleFilter("");
+                    }}
+                  >
+                    {stringManipulation(filter)}{" "}
+                    <i className="bi bi-x-lg text-primary ms-1"></i>
+                  </Button>
+                </Col>
+              )}
+            </Row>
             {isLoadingCars && (
               <Col className="mt-5 d-flex justify-content-center align-items-center">
                 <Spinner animation="border" variant="primary"></Spinner>
               </Col>
             )}
             {carList !== null && (
-              <Row className="justify-content-evenly pt-5">
-                {carList.content.map((car) => {
+              <Row className="justify-content-evenly">
+                {carFilter(carList, filter).map((car) => {
                   return <CarCard key={car.id} car={car} />;
                 })}
               </Row>
